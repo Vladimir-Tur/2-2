93f828057e8ae6f4c09cda60a315bdc2cfa8dc02
Vladimir-Tur <vovanru2033@mail.ru>
2024-09-26 17:24:22 +0400

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+a = int(input('Введите число 1: '))
+b = int(input('Введите число 2: '))
+c = int(input('Введите число 3: '))
+ab = 24
+ac = 68
+ad = 80
+if a==ab and b==ac and c==ad:
+    print(2)
+elif  a==ab and b==ac and c!=ad:
+    print(1)
+else:
+    print(0)
+
